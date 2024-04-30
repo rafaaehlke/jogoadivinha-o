055e6acaa753e912982dca0dbc7e6b4ad8c7bde5
Rafael Ehlke <heeyrafaa@gmail.com>
2024-04-29 22:49:26 -0300

    add alerta para somente numeros entre 0 a 15

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
-const btnTry = document.querySelector("#btnTry");
-const btnAgain = document.querySelector("#btnAgain");
 
-let randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 15);
 let xAtttempts = 1;
 
-//Função callback
 function tryAgain(event) {
   event.preventDefault(); //faz com que nao faça o padrão, enviar o formulário
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    toogleScreen()
+    toggleScreen()
     screen2.querySelector("h2").innerText = `Acertou em ${xAtttempts} Tentativas`;
+  } else if(Number(inputNumber.value) < 0 || Number(inputNumber.value) > 15) {
+     alert(`Digite um numero entre 0 a 15`)
   }
 
   inputNumber.value = ""  //faz com que suma o valor digitado, no input caso não acertado
-  xAtttempts++;
+  xAtttempts++
 }
 
-function btnAgainClick() {
-  toogleScreen()
-  screen2.classList.add("hide");
+function btnAgainClick(){
+  toggleScreen()
   xAtttempts = 1
-  randomNumber = Math.round(Math.random() * 10);
+  randomNumber = Math.round(Math.random() * 10)
 }
 
-function toogleScreen() {
+function toggleScreen() {
   screen1.classList.toggle("hide");
   screen2.classList.toggle("hide");
-  xAtttempts = 1
 }
 
-//Eventos
-btnTry.addEventListener("click", tryAgain);
-btnAgain.addEventListener("click", btnAgainClick)
-document.addEventListener("keydown", function(e) {
-  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
-      tryAgain()
+function keyDown(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    btnAgainClick()
   }
-})
+}
+
+//Eventos 
+const btnTry = document.querySelector("#btnTry");
+const btnAgain = document.querySelector("#btnAgain");
+
+btnTry.addEventListener("click", tryAgain);
+btnAgain.addEventListener("click", btnAgainClick);
+document.addEventListener("keydown", keyDown)
+
